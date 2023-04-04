fdb2d092792c65a0a494283c9f15d8cc54bf9da8
Max <bannmaxx2000@yandex.ru>
2023-04-05 00:23:07 +0300

    Add layout files

--- app/src/main/java/com/forzz/android/newstask/MainActivity.kt
+++ app/src/main/java/com/forzz/android/newstask/MainActivity.kt
@@ -2,10 +2,16 @@ package com.forzz.android.newstask
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_nav_host) as NavHostFragment
+        val navController = navHostFragment.navController
+        NavigationUI.setupActionBarWithNavController(this, navController)
     }
 }
---
+++ app/src/main/java/com/forzz/android/newstask/data/ui/home/NewsListViewModel.kt
@@ -0,0 +1,7 @@
+package com.forzz.android.newstask.data.ui.home
+
+import androidx.lifecycle.ViewModel
+
+class NewsListViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
